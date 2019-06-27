358d366caff9c4fdd4915ad092f6121ed7205246
Jaimen Lathia <JaimenLathia@gmail.com>
2019-06-27 11:53:45 +0100

    chore: clean up profile link from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 
 class Home extends Component {
     render() {
 
-        const { auth } = this.props;
-
         return (
             <React.Fragment>
                 Home
-                {
-                    (auth.isLoggedIn()) ? (
-                        <Link to='/profile' >Profile</Link> ) : (
-                        <button onClick={auth.login}>login</button>
-                    )
-                }
             </React.Fragment>
         );
     }
