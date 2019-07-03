d0b9f9996432c17289f0cc4ea59be16b6e10cdb6
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-07-03 12:55:39 +0100

    refactor: updated courses into functional component

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,35 +1,32 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Courses extends Component {
+const Courses = ({ auth }) => {
 
-    state = {
-        courses: []
-    };
+    const [courses, setCourses] = useState([]);
+    const [error, setError] = useState("");
 
-    componentDidMount() {
-        fetch("/courses", { headers : { Authorization : `Bearer ${this.props.auth.getAccessToken()}` } })
+    useEffect(() => {
+        fetch("/courses", { headers : { Authorization : `Bearer ${auth.getAccessToken()}` } })
             .then(response => {
                 if (response.ok) {
                     return response.json();
                 } else { throw new Error("error with request from server") }
             })
             .then(response => {
-                this.setState({ courses: response.courses });
+                setCourses(response.courses);
             })
             .catch(error => {
-                this.setState({ message: error.message });
+                setError(error.message);
             });
-    }
+    });
 
-    render() {
-        return (
-            <ul>
-                {this.state.courses.map((course) => {
-                    return <li key={course.id}> {course.title} </li>
-                })}
-            </ul>
-        );
-    }
-}
+    return(
+        <ul>
+            {courses.map((course) => {
+                return <li key={course.id}> {course.title} </li>
+            })}
+        </ul>
+    );
+};
 
 export default Courses;
